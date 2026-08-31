a227a98bd85447e8fef0119905e9a8190527350e
Tanuja-Mehra <35681756+Tanuja-Mehra@users.noreply.github.com>
2026-08-31 21:10:30 +0530

    build update

--- src/components/sections/GallerySection.jsx
+++ src/components/sections/GallerySection.jsx
@@ -12,12 +12,38 @@ function GallerySection() {
     dots: false,
     infinite: true,
     speed: 700,
-    slidesToShow: 1,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
     pauseOnHover: true,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   return (
